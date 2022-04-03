b87acd27ab81b28d03dd9322b52622d8df9cc41b
Santhiyavaij <gsanthiya23@gmail.com>
2022-04-03 13:16:41 +0530

    some code added

---
+++ src/gitmain/gitmain.java
@@ -0,0 +1,11 @@
+package gitmain;
+
+public class gitmain {
+
+	public static void main(String[] args) {
+		System.out.println("Employee name is Ben");
+		System.out.println("Employee is is 689");
+		System.out.println("Employee id is 689");
+	}
+
+}
